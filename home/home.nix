3fc9efed02ae6a543977579ef7fdad0e47a44384
FR <137614590+FilipRuman@users.noreply.github.com>
2026-01-11 17:25:34 +0100

    reverted changes

--- home/home.nix
+++ home/home.nix
@@ -5,22 +5,22 @@
   ...
 }: {
   imports = [
-    # ./dunst.nix
-    # ./hypr/main.nix
-    # ./apps/ghostty.nix
-    # ./apps/yazi.nix
+    ./dunst.nix
+    ./hypr/main.nix
+    ./apps/ghostty.nix
+    ./apps/yazi.nix
     # ./stylix/main.nix
   ];
   home.username = "f";
   home.homeDirectory = "/home/f";
   home.stateVersion = "24.11";
 
-  # home.packages = [
-  #   pkgs.nerd-fonts
-  #   pkgs.neovim
-  #   pkgs.git
-  # ];
-  #
+  home.packages = [
+    #   pkgs.nerd-fonts
+    pkgs.neovim
+    pkgs.git
+  ];
+
   home.sessionVariables = {
     EDITOR = "ne";
     BROWSER = "flatpak run app.zen_browser.zen";
